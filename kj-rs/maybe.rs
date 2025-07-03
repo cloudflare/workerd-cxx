0949b6f8576df89fe67bd9f2e20cdb998db7b270
Jimmy Ostler <jostler@cloudflare.com>
2025-07-03 11:02:38 -0500

    [workerd-cxx] Niche value optimization.

--- kj-rs/maybe.rs
+++ kj-rs/maybe.rs
@@ -2,9 +2,71 @@ pub mod repr {
     use std::mem::MaybeUninit;
 
     use static_assertions::assert_eq_size;
+    pub struct Ptr;
 
-    // assert_eq_size!(Maybe<isize>, [usize; 2]);
-    // assert_eq_size!(Maybe<&isize>, usize);
+    pub trait IsNull {
+        fn is_null(&self) -> bool;
+    }
+
+    impl<T> IsNull for *const T {
+        fn is_null(&self) -> bool {
+            <*const T>::is_null(*self)
+        }
+    }
+
+    impl<T> IsNull for *mut T {
+        fn is_null(&self) -> bool {
+            <*mut T>::is_null(*self)
+        }
+    }
+
+    impl<T> IsNull for &T {
+        fn is_null(&self) -> bool {
+            false
+        }
+    }
+
+    impl<T> IsNull for &mut T {
+        fn is_null(&self) -> bool {
+            false
+        }
+    }
+
+    #[repr(C)]
+    pub struct Maybe<T, D = bool> {
+        is_set: D,
+        some: MaybeUninit<T>,
+    }
+
+    impl<T> Maybe<T, bool> {
+        pub fn is_some(&self) -> bool {
+            return self.is_set;
+        }
+
+        pub fn is_none(&self) -> bool {
+            return !self.is_set;
+        }
+    }
+
+    impl<T: IsNull> Maybe<T, Ptr> {
+        pub fn is_some(&self) -> bool {
+            unsafe {
+                return self.some.assume_init_read().is_null();
+            }
+        }
+
+        pub fn is_none(&self) -> bool {
+            return !self.is_some();
+        }
+    }
+
+    // TODO: For when `Own<T>` gets merged
+    // impl<T> Maybe<crate::repr::Own<T>, Ptr> {
+    //     
+    // }
+
+    assert_eq_size!(Maybe<isize>, [usize; 2]);
+    assert_eq_size!(Maybe<&isize, Ptr>, usize);
 
     // #[repr(C, usize)]
     // pub enum Maybe<T> {
@@ -12,39 +74,64 @@ pub mod repr {
     //     Some(T),
     // }
 
-    #[repr(C)]
-    pub struct Maybe<T> {
-        is_set: bool,
-        value: MaybeUninit<T>
-    }
-
-    // impl<T> Maybe<T> {
-        // pub fn is_none(&self) -> bool {
-            // matches!(self, Maybe::None)
-        // }
-
-        // pub fn is_some(&self) -> bool {
-            // matches!(self, Maybe::Some(_))
-        // }
+    // #[repr(C)]
+    // pub struct Maybe<T, D = bool> {
+    //     is_set: D,
+    //     value: MaybeUninit<T>,
     // }
 
-    impl<T> Maybe<T> {
-        pub fn is_none(&self) -> bool {
-            !self.is_set
-        }
+    // impl<T> Maybe<T> {
+    //     pub fn is_none(&self) -> bool {
+    //         matches!(self, Maybe::None)
+    //     }
 
-        pub fn is_some(&self) -> bool {
-            self.is_set
-        }
-    }
+    //     pub fn is_some(&self) -> bool {
+    //         matches!(self, Maybe::Some(_))
+    //     }
+    // }
 
-    impl<T> Drop for Maybe<T> {
-        fn drop(&mut self) {
-            if self.is_set {
-                unsafe {
-                    self.value.assume_init_drop();
-                }
+    // impl<T> Maybe<T> {
+    //     pub fn is_none(&self) -> bool {
+    //         !self.is_set
+    //     }
+
+    //     pub fn is_some(&self) -> bool {
+    //         self.is_set
+    //     }
+    // }
+
+    impl<T> From<Maybe<T>> for Option<T> {
+        fn from(value: Maybe<T>) -> Self {
+            if value.is_some() {
+                Some(value.value)
+            } else {
+                None
             }
         }
     }
+
+    // impl<T> From<Option<T>> for Maybe<T> {
+    //     fn from(value: Option<T>) -> Self {
+    //         match value {
+    //             None => Maybe {
+    //                 is_set: false,
+    //                 value: MaybeUninit::uninit(),
+    //             },
+    //             Some(val) => Maybe {
+    //                 is_set: true,
+    //                 value: MaybeUninit::from(val),
+    //             },
+    //         }
+    //     }
+    // }
+
+    // impl<T> Drop for Maybe<T> {
+    //     fn drop(&mut self) {
+    //         if self.is_set {
+    //             unsafe {
+    //                 self.value.assume_init_drop();
+    //             }
+    //         }
+    //     }
+    // }
 }
